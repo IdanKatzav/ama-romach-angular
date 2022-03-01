c3f62f0cd557d416d9c69478b996ad4397a5b48e
IdanKatzav <idan.katzav@gmail.com>
2022-03-01 10:24:35 +0200

    feat: get products from server

--- src/app/shop/products-in-store.service.ts
+++ src/app/shop/products-in-store.service.ts
@@ -3,15 +3,18 @@ import { Observable } from 'rxjs';
 
 import { Product } from './models/product';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class ProductsInStoreService {
-	private  productsURL: string = '/assets/serverMock/products-in-store-mockup.json';
-	constructor(private httpClient: HttpClient) { }
+	private readonly productsURL: string;
+	constructor(private httpClient: HttpClient) {
+		this.productsURL = environment.server.amaRomachServerAddress + environment.server.productsPath;
+	}
 
-    getProducts():Observable<Product[]>{
+    getProducts$(): Observable<Product[]>{
 		return this.httpClient.get<Product[]>(this.productsURL);
-    }
+	}
 }
